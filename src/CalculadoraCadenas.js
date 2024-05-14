1d0adc8721f4427e6f6e727594875f330ecb30b8
Salvador_Patty_Camacho <salvador.patty@ucb.edu.bo>
2024-05-14 00:22:30 -0400

    Refactoring de la funcion Calculadora_de_Cadena

--- src/CalculadoraCadenas.js
+++ src/CalculadoraCadenas.js
@@ -1,18 +1,17 @@
 function Calculadora_de_Cadena(cadena) {
+    if(cadena === ""){
+        return 0;
+    }
+
     let resultado = 0;
-    if(cadena == "")
-    {
-        resultado = 0;
+    const numeros = cadena.split(',');
+
+    for (let i = 0; i < numeros.length; i++) {
+        resultado = resultado + parseInt(numeros[i]);
+        console.log(numeros[i])
     }
-    else
-    {
-        let numeros = cadena.split(',');
-        for (let i = 0; i < numeros.length; i++) {
-            resultado = resultado + parseInt(numeros[i]);
-            console.log(numeros[i])
-        }
-    }
+    
     return resultado;
-  }
+}
 
   export default Calculadora_de_Cadena;
